e93d1653c3fe754fcf058b59567eccb7047f01de
Djordje Senicic <x0157990@ti.com>
2019-09-27 15:52:49 -0400

    mcbench: Add MobileNetV2 test cases
    - PLSDK-3078
    
    Signed-off-by: Djordje Senicic <x0157990@ti.com>

--- examples/mcbench/scripts/all_5728.sh
+++ examples/mcbench/scripts/all_5728.sh
@@ -1,4 +1,5 @@
-./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_mobileNet1.txt -f 50 -i ../test/testvecs/input/preproc_0_224x224_multi.y
+./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_mobileNet1.txt -f 50 -i ../test/testvecs/input/preproc_2_224x224_multi.y
+./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_mobileNet2.txt -f 50 -i ../test/testvecs/input/preproc_2_224x224_multi.y
 ./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_squeeze1_1.txt -f 50 -i ../test/testvecs/input/preproc_1_227x227_multi.y
 ./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_inceptionNetv1.txt -f 50 -i ../test/testvecs/input/preproc_0_224x224_multi.y
 ./mcbench -g 1 -d 2 -e 0 -c ../test/testvecs/config/infer/tidl_config_j11_v2.txt -f 50 -i ../test/testvecs/input/preproc_0_224x224_multi.y
